c45f556120b00e68213c714002eb8e9bb5c0f193
Dominic Amato <dominic.a.amato@gmail.com>
2017-01-18 14:24:45 -0600

    use FTP helper class instead

--- proxy/Server.java
+++ proxy/Server.java
@@ -1,13 +1,6 @@
 package com.dyn.achievements.proxy;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.time.LocalDate;
-
-import org.apache.commons.net.PrintCommandListener;
-import org.apache.commons.net.ftp.FTPClient;
 
 import com.dyn.DYNServerMod;
 import com.dyn.achievements.achievement.AchievementPlus;
@@ -18,7 +11,7 @@ import com.dyn.achievements.achievement.Requirements.PickupRequirement;
 import com.dyn.achievements.handlers.AchievementManager;
 import com.dyn.achievements.handlers.EventHandler;
 import com.dyn.server.database.DBManager;
-import com.dyn.server.keys.KeyManager;
+import com.dyn.utils.FTPHelper;
 import com.dyn.utils.FileUtils;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
@@ -26,7 +19,6 @@ import com.google.gson.JsonObject;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.common.FMLCommonHandler;
 
 public class Server implements Proxy {
 
@@ -40,55 +32,13 @@ public class Server implements Proxy {
 			JsonObject achievementJson = DBManager.getAchievementDBAsJson();
 
 			File achievementFile = new File(MinecraftServer.getServer().getDataDirectory(),
-					"current_achievements_" + LocalDate.now() + ".json");
+					"current_achievements.json"); // _" + LocalDate.now() + "
 			FileUtils.createJsonFile(achievementJson, achievementFile);
 
 			if (!DYNServerMod.developmentEnvironment && DYNServerMod.apacheNetLoaded
 					&& DYNServerMod.ftpConnectionOpen) {
 				// upload to ftp, thread it so it doesnt block
-				FMLCommonHandler.instance().getMinecraftServerInstance().addScheduledTask(() -> {
-					FTPClient client = new FTPClient();
-					client.addProtocolCommandListener(new PrintCommandListener(new PrintWriter(System.out)));
-					FileInputStream fis = null;
-
-					try {
-						DYNServerMod.logger.info("Connecting to FTP");
-						client.connect(KeyManager.getFtpKeys().getLeft());
-						DYNServerMod.logger.info("Logging in to FTP");
-						if (client.login(KeyManager.getFtpKeys().getMiddle(), KeyManager.getFtpKeys().getRight())) {
-
-							// Create an InputStream of the file to be uploaded
-							fis = new FileInputStream(achievementFile);
-							// Store file to server
-							DYNServerMod.logger.info("Uploading " + achievementFile.getName()
-									+ " to FTP server at /Minecraft/" + achievementFile.getName());
-
-							if (!client.storeFile("/Minecraft/" + achievementFile.getName(), fis)) {
-								DYNServerMod.logger.error("Failed to upload file");
-							} else {
-								DYNServerMod.logger.info("Deleting old file");
-								client.deleteFile("/Minecraft/current_achievements.json");
-								DYNServerMod.logger.info("Renaming to current_achievements.json");
-								client.rename("/Minecraft/" + achievementFile.getName(),
-										"/Minecraft/current_achievements.json");
-							}
-							fis.close();
-						} else {
-							DYNServerMod.logger.error("Could not log in to ftp client");
-						}
-					} catch (IOException e) {
-						DYNServerMod.logger.error("Failed during achievement upload", e);
-					} finally {
-						try {
-							if (fis != null) {
-								fis.close();
-							}
-							client.disconnect();
-						} catch (IOException e) {
-							DYNServerMod.logger.error("Failed tring to close stream or ftp client", e);
-						}
-					}
-				});
+				FTPHelper.uploadFile(achievementFile, "/Minecraft/");
 			}
 
 			for (JsonElement ach : achievementJson.get("achievements").getAsJsonArray()) {
